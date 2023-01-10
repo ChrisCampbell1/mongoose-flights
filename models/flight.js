d917b9d175c7b988770882ed6907805afab5c976
Chris Campbell <christopherecampbell@gmail.com>
2023-01-10 16:47:37 -0700

    define ticket schema and add to flight schema

--- models/flight.js
+++ models/flight.js
@@ -3,6 +3,17 @@ import mongoose, { mongo } from 'mongoose'
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
 
+const ticketSchema = new Schema({
+  seat: {
+    type: String,
+    match: /[A-F][1-9]\d?/
+  },
+  price: {
+    type: Number,
+    min: 0
+  }
+})
+
 const flightSchema = new Schema ({
   airline: {
     type: String,
@@ -26,7 +37,8 @@ const flightSchema = new Schema ({
       date.setFullYear(oneYearFromNow)
       return date
     }
-  }
+  },
+  tickets: [ticketSchema]
 })
 
 const Flight = mongoose.model('Flight', flightSchema)
