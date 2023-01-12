38130994913cdefa872684b9114031aad6427121
Chris Campbell <christopherecampbell@gmail.com>
2023-01-11 17:32:12 -0700

    add meal view and route/controller

---
+++ routes/meals.js
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import * as mealsCtrl from '../controllers/meals.js'
+
+const router = Router()
+
+/* GET users listing. */
+router.get('/new', mealsCtrl.new)
+
+
+export {
+  router
+}
